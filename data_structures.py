7a431151bc9ac032dd8cdc8a4b6cea2d31363faa
Pragya Swami <64581064+Prags31@users.noreply.github.com>
2021-08-28 18:05:50 +0530

    Create data_structures.py
    Set Operations

---
+++ data_structures.py
@@ -0,0 +1,10 @@
+A = {0, 2, 4, 6, 8}
+B = {1, 2, 3, 4, 5}
+#union output
+print("Union is- ", A|B)
+#intersection output
+print("Intersection is- ", A&B)
+#difference output
+print("Difference is- ", A-B)
+#symmetric difference output
+print("Symmetric difference is- ", A^B)
